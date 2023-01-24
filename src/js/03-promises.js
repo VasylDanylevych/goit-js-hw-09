01298fefd1810de554add90299252113fc6d5313
VasylDanylevych <ukrainec31@gmail.com>
2023-01-25 00:19:01 +0200

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const submitBtn = document.querySelector('button');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+submitBtn.addEventListener('click', onSubmitBtnClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+function onSubmitBtnClick(evt) {
+  evt.preventDefault();
+    for (let position = 1; position <= amount.value; position += 1) {
+      createPromise(position, delay.value)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        })
+      delay.value += step.value;
+    }
+  };
+
+
